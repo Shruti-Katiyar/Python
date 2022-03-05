5ecd96557d37c3b060cc880a9bdb2c49f61d915b
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-05 16:35:04 +0530

    py

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+rating=4.3
+print(rating , type(rating))
+
+name='she'
+marks=33
+
+is_passed=True
+list=['apple', 'mango', 'kiwi']
+
+
+
+print(name ,type(name))
+print(marks ,type(marks))
+print(list ,type(list))
