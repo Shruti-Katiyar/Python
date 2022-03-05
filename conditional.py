8ef2ee84f381e534504d3d2420e6accd88754866
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-05 16:33:41 +0530

    py

---
+++ conditional.py
@@ -0,0 +1,18 @@
+#if else statement
+
+# age=12
+# if age > 18:
+#    print("you can vote")
+# else:
+#     print("no")
+
+#if else  multiple times krna ho to we use (elif)
+
+num=-4
+
+if num>0:
+     print("positive no")
+elif num==0:
+    print("zero")
+else:
+     print("negative no")   
