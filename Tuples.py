facd478c38f081f021164a4bdb79f0fda713317f
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-05 16:33:24 +0530

    py

---
+++ Tuples.py
@@ -0,0 +1,16 @@
+#they are same as list but list are mutable but tuples are not they can not be changed once they are assigned
+#tuples ans string are  immutable 
+#tuples have ()
+
+a=('a','b','c')
+print(a)
+
+
+#operations and functions are same as list
+# len(tuple(a))
+# max(tuple(a))
+# min(tuple(a))
+# # tuple(seq)
+# # sum(tuple)
+
+
