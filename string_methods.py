1a61eff5852eb86bd399342145a2073f21738803
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-06 23:53:22 +0530

    py

---
+++ string_methods.py
@@ -0,0 +1,26 @@
+# solve="abcdseft"
+# print(solve.isalpha())
+#  o/p is true bw isalpah()gives true if all the char are letters
+
+# a="absbd456"
+# print(a.isalpha())
+#output will be false
+
+# s="23455"
+# print(s.isdigit()) 
+#isdigit fun will give true when all the no are there
+
+s="abcd"
+print('s.isalpha' , s.isalpha())
+print('s.isdigit' , s.isdigit())
+print('s.islower' , s.islower())
+print('s.isupper' , s.isupper())
+
+
+print('s.upper' , s.upper()) #this will change lower char to upper
+print('s.lower' , s.lower()) #upper to lower
+
+y='    ancd      '
+print(y.lstrip()) #ye left side say space ko remove krta h
+print(y.rstrip()) #ye right side ka krta h
+
