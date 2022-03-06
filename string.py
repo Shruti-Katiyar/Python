00415c3d40422b9edac856c409285f9442617d6b
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-06 23:53:03 +0530

    py

---
+++ string.py
@@ -0,0 +1,35 @@
+# name="shivu"
+# par='''this is 
+# a car
+# moving'''
+
+# print(par) # 3"""" or 3''' multiline string k liye use hota h
+
+#indexing in string
+
+# fruit="apple"
+# print(fruit[0])
+# print(fruit[4])
+# print(fruit[-1]) #-1 means  last char 
+# # 0 1 2 3 4
+# #-5 -4 -3 -2 -1
+# print(fruit[-2])
+
+# a=fruit[2]
+# print(a)
+
+#we can add string also
+
+# a="abcd"
+# b="edf"
+# # c=a+b
+# c=a*4
+# print(c)
+
+#substring in python called as Slicing
+
+#for loop in strings
+
+a="abc"
+for solve in a:
+    print(solve*2)
