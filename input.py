fee115ea2dd558fdd1c434d5bfb3d4247c9c6c25
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-08 17:49:42 +0530

    py

---
+++ input.py
@@ -0,0 +1,20 @@
+# a=input()
+# b=input()
+
+# c=a*b
+# print(c)
+
+# this will give error bw  by default input() considered as string 
+# to convert it into we have to typecast it
+
+# a=int(input())
+# b=int(input())
+
+a=int(input('enter a'))
+b=int(input('enter b'))
+
+
+c=a*b
+
+print(c)
+
