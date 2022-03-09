5915a2e36651a3c00a49c34bc1531d88028f86a8
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-09 12:04:03 +0530

    py

---
+++ list.py
@@ -0,0 +1,24 @@
+# my_list=[1,2,3]
+# print(my_list)
+
+# x=[1,2,3,"a","b","c"]
+# print(x)
+
+# name="shruti"
+# name=123 #python m type define nh hota h variable ka isly char ya int sab dal skatay h jo last updated hogha whi print hota h
+
+# print(name)
+
+fruits= ['apple','banana','kiwi']
+print(fruits[0]) #this will print apple  bw in 0th idex apple is there
+print(fruits[2])#kiwi (AND -v indexing is also supported in string or list)
+
+#list are mutable we can update elements in list
+name=['rashi','ritik','nital']
+name[1]='shruti' #this will change 1st index name 
+print(name)
+
+#we can delete also in list 
+del name[0] #0th idex name will deleted
+del name #it will delete whole list of names
+print(name)
