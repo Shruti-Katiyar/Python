b72991468b0cafa2ec28e91bff914bdbb585ca31
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-10 19:49:00 +0530

    py

---
+++ list_function.py
@@ -0,0 +1,19 @@
+#len(list)
+a=[1,3,4,6,8,9]
+print(len(a))
+
+
+#max(list)
+print(max(a))
+
+
+#min(list)
+print(min(a))
+
+
+#list(seq)  this will typecast any seq in list
+s="abc"
+print(list(s))
+
+#sum(list) 
+print(sum(a))
