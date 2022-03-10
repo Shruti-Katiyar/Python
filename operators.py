d4e60f07e82684fe0c7d951b9199fc300cdb6fbd
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-10 19:48:39 +0530

    py

---
+++ operators.py
@@ -0,0 +1,33 @@
+# a=5
+# b=13
+
+# # c=a+b
+# # print(c)
+
+# # print(a+b)
+# # print(a-b)
+# # print(a*b)
+# # print(a/b)
+# # print(a%b)
+
+# '''
+# print(a//b)
+# print(a**b)
+
+# '''
+ 
+# print(a>b)
+# print('a<b' ,a<b)
+# print(a>=b)
+
+
+x=True
+y=False
+
+print('x and y' , x and y)
+
+print(x or y)
+
+print(not x)
+
+
