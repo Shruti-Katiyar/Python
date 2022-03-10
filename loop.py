27bc04088a09a8bbd4f4141e639d5d3d16308270
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-10 19:49:15 +0530

    py

---
+++ loop.py
@@ -0,0 +1,28 @@
+# for x in range(10):
+
+    # print(2*x)
+    # print(x)
+
+    # print(2*x , end=", ") #by default end m new line dal daytai h but hm kuch or fun bhi kra skaty h
+
+#  a=['shruti' , 'rashi ', 'shivi']
+#  for name in a:
+#     print(name*4)
+
+#while loop
+
+# n=5
+# while (n>=0):
+#    print(n)
+#    n-=1
+
+
+# for i in range(10):
+#   if i>6:
+#       break
+#   print(i)
+
+for i in range(10):
+    if i == 6:
+        continue
+    print(i)
